d5bf5d737adfeffc5ecb517399585924657e492f
Benoit Gantaume <benoit@agilidee.com>
2021-11-27 08:28:26 +0100

    Start analysing metadatas

--- src/nftStats.ts
+++ src/nftStats.ts
@@ -1,18 +1,29 @@
-import fs from "fs";
+import fs from "fs"
 
-let metadatas = "./assets/NotRichYet/metadatas/";
+let metadatas = "./assets/Sample2/metadatas/"
 
 const files = fs.readdirSync(metadatas)
+let stats : Map<string, number> = new Map<string, number>()
 
 for (const file of files) {
-  console.log(file)
   fs.readFile(metadatas+"/"+file , function (err, data) {
     if (err) {
-        return console.error(err);
+        return console.error(err)
     } else {
-      metadatas = JSON.parse(data.toString());
-      console.log(metadatas);
-    }
+      metadatas = JSON.parse(data.toString())
+      let name = metadatas["name"];
+      console.log(stats[name])
+      if (stats[name] === undefined) {
+        stats[name] = 1
+      } else {
+        stats[name] += 1
+      }
+      for(let attribute of metadatas["attributes"]) {
+        console.log(attribute["trait_type"] + "->" + attribute["value"])
+      }
 
-});
+    }
+    console.log(stats)
+  });
+
 }
